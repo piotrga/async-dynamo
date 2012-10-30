1eb864b059fa8b0f9d83f356df3a18a6849e61de
Piotr Gabryanczyk <piotrga@gmail.com>
2012-10-30 20:19:09 +0000

    new operations: Query DeleteByRange

--- src/main/scala/asyncdynamo/nonblocking/operations.scala
+++ src/main/scala/asyncdynamo/nonblocking/operations.scala
@@ -8,7 +8,9 @@ import com.amazonaws.services.dynamodb.model.PutItemRequest
 import com.amazonaws.services.dynamodb.model.ScanRequest
 import com.amazonaws.services.dynamodb.model.DeleteItemRequest
 import com.amazonaws.services.dynamodb.model.Key
-import asyncdynamo.{DynamoObject, DbOperation}
+import asyncdynamo.{functional, DynamoObject, DbOperation}
+import akka.actor.ActorRef
+import akka.util.Timeout
 
 
 case class Save[T : DynamoObject](o : T) extends DbOperation[T]{
@@ -55,5 +57,57 @@ case class DeleteById[T](id: String)(implicit dyn:DynamoObject[T]) extends DbOpe
   }
 }
 
+case class DeleteByRange[T](id: String, range: Any, expected: Map[String,String] = Map.empty)(implicit dyn:DynamoObject[T]) extends DbOperation[Unit]{
+  def execute(db: AmazonDynamoDBClient, tablePrefix:String){
+    val rangeAttribute = if (dyn.range.get.getAttributeType == "S")
+      new AttributeValue().withS(range.toString)
+    else
+      new AttributeValue().withN(range.toString)
 
+    val key = new Key()
+      .withHashKeyElement(new AttributeValue(id))
+      .withRangeKeyElement(rangeAttribute)
+
+    val request = new DeleteItemRequest()
+      .withTableName(dyn.table(tablePrefix))
+      .withKey(key)
+      .withExpected(expected.map{case (k,v)=> (k, new ExpectedAttributeValue(new AttributeValue(v.toString)))}.asJava)
+
+    db.deleteItem( request)
+  }
+}
+
+case class Query[T](id: String, operator: String, attributes: Seq[String], limit : Int = Int.MaxValue, exclusiveStartKey: Key = null, consistentRead :Boolean = true)(implicit dyn:DynamoObject[T]) extends DbOperation[(Seq[T], Option[Key])]{
+  def execute(db: AmazonDynamoDBClient, tablePrefix:String) : (Seq[T], Option[Key]) = {
+
+    val condition = new Condition()
+      .withComparisonOperator(operator)
+      .withAttributeValueList(attributes.map(new AttributeValue(_)).asJava)
+
+    val query = new QueryRequest()
+      .withTableName(dyn.table(tablePrefix))
+      .withHashKeyValue(new AttributeValue(id))
+      .withRangeKeyCondition(condition)
+      .withExclusiveStartKey(exclusiveStartKey)
+      .withConsistentRead(consistentRead)
+      .withLimit(limit)
+
+    val result = db.query(query)
+    val items = result.getItems.asScala.map {
+      item => dyn.fromDynamo(item.asScala.toMap)
+    }
+
+    (items, Option(result.getLastEvaluatedKey))
+  }
+
+  def blockingStream(implicit dynamo: ActorRef, pageTimeout: Timeout): Stream[T] =
+    functional.unfold[Query[T], Seq[T]](this){
+      query =>
+        val (resultChunk, lastKey) = query.blockingExecute
+        lastKey match{
+          case None => (None, resultChunk)
+          case Some(key) => (Some(query.copy(exclusiveStartKey = key)), resultChunk)
+        }
+    }.flatten
+}
 
