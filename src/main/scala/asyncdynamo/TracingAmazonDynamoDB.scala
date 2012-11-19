dbcbe6e5b5d6169b48f54b82c131416b568bea84
Piotr Gabryanczyk <piotrga@gmail.com>
2012-11-19 13:18:30 +0000

    tracing tablename - silly...

--- src/main/scala/asyncdynamo/TracingAmazonDynamoDB.scala
+++ src/main/scala/asyncdynamo/TracingAmazonDynamoDB.scala
@@ -8,58 +8,55 @@ import akka.event.EventStream
 private class TracingAmazonDynamoDB(delegate  : AmazonDynamoDB, eventStream : EventStream) extends AmazonDynamoDB {
 
   def setEndpoint(endpoint: String) {delegate.setEndpoint(endpoint)}
-  def updateTable(updateTableRequest: UpdateTableRequest) = delegate.updateTable(updateTableRequest)
   def getCachedResponseMetadata(request: AmazonWebServiceRequest) = delegate.getCachedResponseMetadata(request)
+
+  def createTable(createTableRequest: CreateTableRequest) = delegate.createTable(createTableRequest)
+  def updateTable(updateTableRequest: UpdateTableRequest) = delegate.updateTable(updateTableRequest)
+  def describeTable(describeTableRequest: DescribeTableRequest) = delegate.describeTable(describeTableRequest)
   def listTables() = delegate.listTables()
   def listTables(listTablesRequest: ListTablesRequest) = delegate.listTables(listTablesRequest)
   def deleteTable(deleteTableRequest: DeleteTableRequest) = delegate.deleteTable(deleteTableRequest)
-  def describeTable(describeTableRequest: DescribeTableRequest) = delegate.describeTable(describeTableRequest)
+
   def shutdown() {delegate.shutdown()}
 
   import Operation._
-
-  def createTable(createTableRequest: CreateTableRequest) = {
-    val res = delegate.createTable(createTableRequest)
-    pub(DynamoRequestExecuted(Operation(Admin,"CreateTable"), 0, 0))
-    res
-  }
 
 
 
   def deleteItem(deleteItemRequest: DeleteItemRequest) = {
     val res = delegate.deleteItem(deleteItemRequest)
-    pub(DynamoRequestExecuted(Operation(Write,"DeleteItem"), 0, res.getConsumedCapacityUnits))
+    pub(DynamoRequestExecuted(Operation(deleteItemRequest.getTableName, Write,"DeleteItem"), writeUnits = res.getConsumedCapacityUnits))
     res
   }
 
   def getItem(getItemRequest: GetItemRequest) = {
     val res = delegate.getItem(getItemRequest)
-    pub(DynamoRequestExecuted(Operation(Read,"GetItem"), res.getConsumedCapacityUnits, 0))
+    pub(DynamoRequestExecuted(Operation(getItemRequest.getTableName, Read,"GetItem"), readUnits = res.getConsumedCapacityUnits))
     res
   }
 
   def scan(scanRequest: ScanRequest) = {
     val res = delegate.scan(scanRequest)
-    pub(DynamoRequestExecuted(Operation(Read,"Scan"), res.getConsumedCapacityUnits, 0))
+    pub(DynamoRequestExecuted(Operation(scanRequest.getTableName, Read,"Scan"), readUnits = res.getConsumedCapacityUnits))
     res
   }
 
 
   def updateItem(updateItemRequest: UpdateItemRequest) = {
     val res = delegate.updateItem(updateItemRequest)
-    pub(DynamoRequestExecuted(Operation(Write,"UpdateItem"), res.getConsumedCapacityUnits, 0))
+    pub(DynamoRequestExecuted(Operation(updateItemRequest.getTableName, Write,"UpdateItem"), writeUnits = res.getConsumedCapacityUnits))
     res
   }
 
   def query(queryRequest: QueryRequest) = {
     val res = delegate.query(queryRequest)
-    pub(DynamoRequestExecuted(Operation(Read,"Query"), res.getConsumedCapacityUnits, 0))
+    pub(DynamoRequestExecuted(Operation(queryRequest.getTableName, Read,"Query"), readUnits = res.getConsumedCapacityUnits))
     res
   }
 
   def putItem(putItemRequest: PutItemRequest) = {
     val res = delegate.putItem(putItemRequest)
-    pub(DynamoRequestExecuted(Operation(Write,"PutItem"), 0, res.getConsumedCapacityUnits))
+    pub(DynamoRequestExecuted(Operation(putItemRequest.getTableName, Write,"PutItem"), writeUnits = res.getConsumedCapacityUnits))
     res
   }
 
@@ -68,7 +65,9 @@ private class TracingAmazonDynamoDB(delegate  : AmazonDynamoDB, eventStream : Ev
 
   def batchGetItem(batchGetItemRequest: BatchGetItemRequest) = {
     val res = delegate.batchGetItem(batchGetItemRequest)
-    pub(DynamoRequestExecuted(Operation(Read,"BatchGetItem"), res.getResponses.values map(_.getConsumedCapacityUnits.toDouble) sum, 0))
+    res.getResponses foreach {case (tableName, r) =>
+      pub(DynamoRequestExecuted(Operation(tableName, Read, "BatchGetItem"), readUnits = r.getConsumedCapacityUnits.toDouble))
+    }
     res
   }
 
