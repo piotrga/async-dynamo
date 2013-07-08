709e93ea5c056b440ef5e92bc9e9fcff7b453097
Alex Dean <alexander.dean@keplarllp.com>
2013-07-08 21:54:37 +0300

    Moved to standard Snowplow configuration ready for release
    Conflicts:
    	build.sbt
    	project/plugins.sbt
    	release.sbt
    	version.sbt

---
+++ project/BuildSettings.scala
@@ -0,0 +1,31 @@
+import sbt._
+import Keys._
+
+object BuildSettings {
+
+  // Basic settings for our app
+  lazy val basicSettings = Seq[Setting[_]](
+    organization  := "asyncdynamo",
+    version       := "1.7.1",
+    description   := "Asynchronous Scala client for Amazon DynamoDB",
+    scalaVersion  := "2.10.1",
+    scalacOptions := Seq("-deprecation", "-encoding", "utf8"),
+    resolvers     ++= Dependencies.resolutionRepos
+  )
+
+  // Publish settings
+  // TODO: update with ivy credentials etc when we start using Nexus
+  lazy val publishSettings = Seq[Setting[_]](
+   
+    crossPaths := false,
+    publishTo <<= version { version =>
+      val keyFile = (Path.userHome / ".ssh" / "admin_keplar.osk")
+      val basePath = "/var/www/maven.snplow.com/prod/public/%s".format {
+        if (version.trim.endsWith("SNAPSHOT")) "snapshots/" else "releases/"
+      }
+      Some(Resolver.sftp("SnowPlow Analytics Maven repository", "prodbox", 8686, basePath) as ("admin", keyFile))
+    }
+  )
+
+  lazy val buildSettings = basicSettings ++ publishSettings
+}
--- version.sbt
+++
@@ -1,2 +0,0 @@
-
-version in ThisBuild := "1.6.1-SNAPSHOT"
