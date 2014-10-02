00d43358e3d66a525f064b619485a7c0f9a5e01f
tim <glidester@gmail.com>
2014-10-02 16:13:26 +0100

    delete test tables generated by unit tests

---
+++ resources/delete_dynamo_test_tables.sh
@@ -0,0 +1,5 @@
+aws dynamodb delete-table --table-name devng_tim_dynamotest --region eu-west-1
+aws dynamodb delete-table --table-name devng_tim_dynamotest_with_global_secondary_index --region eu-west-1
+aws dynamodb delete-table --table-name devng_tim_dynamotest_with_local_secondary_index --region eu-west-1
+aws dynamodb delete-table --table-name devng_tim_dynamotest_with_numeric_range --region eu-west-1
+aws dynamodb delete-table --table-name devng_tim_dynamotest_with_range --region eu-west-1
